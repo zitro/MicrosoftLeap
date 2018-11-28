038fc5d4b9d19134f380c3007dfb1cb57c089699
zitro <bryan@bryanortiz.com>
2018-11-28 08:48:16 -0800

    added Inheritance and Polymorphism lab

---
+++ algoPracticeStrings/algoPracticeStrings/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace algoPracticeStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] myArr = { "b", "r", "y", "a", "n" };
+            Console.WriteLine(reverse(myArr));
+
+           
+            
+        }
+
+        private static void reverse(char[] myArr)
+        {
+            char[] newArr = new char[5];
+            for (int i = 0, j = myArr.Length - 1; i < myArr.Length; i++, j--)
+            {
+                newArr[i] = myArr[j];
+            }
+            
+            newArr.ToString;
+        }
+    }
+}
