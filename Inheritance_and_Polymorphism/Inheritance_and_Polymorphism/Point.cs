50ca797eb7421a72c3a1c776dabb3acceafed33c
zitro <bryan@bryanortiz.com>
2018-11-28 09:05:01 -0800

    added in point.cs file and added the unit test ref to main proj

---
+++ Inheritance_and_Polymorphism/Inheritance_and_Polymorphism/Point.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inheritance_and_Polymorphism
+{
+    public class Point
+    {
+        public double X { get; protected set; }
+        public double Y { get; protected set; }
+
+        public Point(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+
+}
