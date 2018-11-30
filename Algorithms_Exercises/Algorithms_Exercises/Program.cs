08cdaa2c6e7ba39f88f67b6a319d07456b7d81d9
zitro <bryan@bryanortiz.com>
2018-11-30 11:11:34 -0800

    added algo exercises

---
+++ Algorithms_Exercises/Algorithms_Exercises/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Algorithms_Exercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
