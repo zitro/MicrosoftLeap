898cbe4151e4c8ca615d14f8d1b8c9ea78425aca
zitro <bryan@bryanortiz.com>
2018-11-30 12:18:46 -0800

    added wheel of fortune: wip

--- Algorithms_Exercises/Algorithms_Exercises/Program.cs
+++ Algorithms_Exercises/Algorithms_Exercises/Program.cs
@@ -5,8 +5,45 @@ namespace Algorithms_Exercises
     class Program
     {
         static void Main(string[] args)
+
         {
-            Console.WriteLine("Hello World!");
+
+            List<int> myList = new List<int>(new int[] { 2, 3, 7 });
+            
+            LargestTwoElementConsecutiveSum(myList);
         }
+
+        private static void LargestTwoElementConsecutiveSum(List<int> [] arr)
+        {
+            int first, second;
+            if (arr[0] > arr[1])
+            {
+                first = arr[0];
+                second = arr[1];
+            }
+            else
+            {
+                first = arr[1];
+                second = arr[0];
+            }
+
+
+            for (int i = 2; i < arr.Length; i++)
+            {
+
+                if (arr[i] > first)
+                {
+                    second = first;
+                    first = arr[i];
+                }
+
+
+                else if (arr[i] > second &&
+                         arr[i] != first)
+                    second = arr[i];
+            }
+            return (first + second);
+        }
+    }
     }
 }
